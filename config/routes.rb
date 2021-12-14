5aee9c383cb2d8e31e70a1610c167564dbb0674a
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-14 15:13:03 +0000

    [次view作る]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books,only: [:show,:index,:create,:edit,:update,:destroy] do
+    resource :favorites, only: [:create,:destroy]
+  end
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
 end
