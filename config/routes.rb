703cff2f9808979e8a1cad3827acc6ff9365febf
goto0214 <yuusuke0214goto1997@outlook.jp>
2021-12-17 07:09:19 +0000

    [finish4]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :users,only: [:show,:index,:edit,:update] do
+    resources :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   resources :books,only: [:show,:index,:create,:edit,:update,:destroy] do
     resources :book_comments, only: [:create,:destroy]
     resource :favorites, only: [:create,:destroy]
