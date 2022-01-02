c660c25473c7233eaa9b22e452cff4bf0b027525
goto0214 <yuusuke0214goto1997@outlook.jp>
2022-01-02 09:27:29 +0000

    [update]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_16_133948) do
+ActiveRecord::Schema.define(version: 2022_01_02_073331) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_12_16_133948) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "evaluation"
   end
 
   create_table "favorites", force: :cascade do |t|
